a3f7a72fdc7e64474bb00937b903c0a7d03559a2
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2022-02-24 12:07:24 +0900

    Update server.js

--- express/server.js
+++ express/server.js
@@ -1,22 +1,26 @@
-'use strict';
 const express = require('express');
 const path = require('path');
-const serverless = require('serverless-http');
 const app = express();
+const serverless = require('serverless-http');
+const server = serverless(app);
 const bodyParser = require('body-parser');
+const io = require('socket.io')(server);
+
+io.set('transports', ['websocket']);
+const port = process.env.PORT || 5000;
+
+let rooms = {};
+let playerTable = {};
+let NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+  res.sendfile(join(__dirname, '../public', 'index.html'));
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../public', 'index.html')));
 
 module.exports = app;
-module.exports.handler = serverless(app);
+module.exports.handler = server;
